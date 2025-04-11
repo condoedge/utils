014deaa73a28f909766141ec94d76aa67bbf2855
Ezequiel <tavelliezequiel@gmail.com>
2025-04-11 20:09:50 -0300

    Fix components loader

--- resources/js/components-loader.js
+++ resources/js/components-loader.js
@@ -1,5 +1,5 @@
-const files = require.context(path, true, /^Vl[a-zA-Z0-9]+\.vue$/i)
+const files = require.context('./components', true, /\.vue$/i)
 
 files.keys().map(key => {
-    Vue.component('Vl'+key.split('/').pop().split('.')[0], files(key).default)
+    Vue.component(key.split('/').pop().split('.')[0], files(key).default)
 })
