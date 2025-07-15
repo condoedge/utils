a4511e30abdd6cf0405d13926e17066d1a52ab9c
basslhm <contact@kompo.io>
2025-07-15 15:50:24 -0300

    Fix file route previews

--- src/Kompo/Files/ImagePreview.php
+++ src/Kompo/Files/ImagePreview.php
@@ -6,6 +6,6 @@ class ImagePreview extends AbstractPreview
 {
 	public function render()
 	{
-		return _Img($this->model->name)->src(fileRoute($this->model->fileType, $this->model->id))->class('max-h-screen');
+		return _Img($this->model->name)->src(fileRoute($this->fileType, $this->model->id))->class('max-h-screen');
 	}
 }
