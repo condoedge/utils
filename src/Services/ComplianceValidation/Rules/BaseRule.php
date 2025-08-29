28545240d5b9c58f34df205c95c95cf6d89255f4
Ezequiel <tavelliezequiel@gmail.com>
2025-08-29 08:15:07 -0300

    Minor refactor to make BaseRule clearer

--- src/Services/ComplianceValidation/Rules/BaseRule.php
+++ src/Services/ComplianceValidation/Rules/BaseRule.php
@@ -13,12 +13,21 @@ abstract class BaseRule implements RuleContract
     public function runValidation(): ValidationExecution
     {
         $startedAt = now();
-
         [$failingValidatables, $testedCount] = $this->findViolations();
+        
+        $complianceIssuesData = $this->createComplianceIssuesData($failingValidatables);
+        $newIssues = $this->filterNewIssues($complianceIssuesData);
+        $this->insertNewIssues($newIssues);
+        $this->resolveFixedIssues($failingValidatables);
+        
+        return $this->createExecutionRecord($startedAt, $testedCount, $failingValidatables);
+    }
 
+    protected function createComplianceIssuesData(array $failingValidatables): \Illuminate\Support\Collection
+    {
         $now = now()->format('Y-m-d H:i:s');
         
-        $complianceIssues = collect($failingValidatables)
+        return collect($failingValidatables)
             ->map(function (ValidatableContract $validatable) use ($now) {
                 $complianceIssue = $validatable->getFailedValidationObject();
                 $complianceIssue->detected_at = $now;
@@ -33,7 +42,10 @@ abstract class BaseRule implements RuleContract
                 
                 return $data;
             });
+    }
 
+    protected function filterNewIssues(\Illuminate\Support\Collection $complianceIssues): array
+    {
         $existingKeys = ComplianceIssue::where('rule_code', static::class)
             ->whereIn('validatable_id', $complianceIssues->pluck('validatable_id'))
             ->whereIn('validatable_type', $complianceIssues->pluck('validatable_type'))
@@ -42,22 +54,31 @@ abstract class BaseRule implements RuleContract
             ->map(fn($item) => $item->validatable_type . '_' . $item->validatable_id)
             ->toArray();
 
-        $newComplianceIssues = $complianceIssues
+        return $complianceIssues
             ->filter(function($issue) use ($existingKeys) {
                 $key = $issue['validatable_type'] . '_' . $issue['validatable_id'];
                 return !in_array($key, $existingKeys);
             })
             ->toArray();
+    }
 
-        if (!empty($newComplianceIssues)) {
-            ComplianceIssue::insert($newComplianceIssues);
+    protected function insertNewIssues(array $newIssues): void
+    {
+        if (!empty($newIssues)) {
+            ComplianceIssue::insert($newIssues);
         }
+    }
 
+    protected function resolveFixedIssues(array $failingValidatables): void
+    {
         ComplianceIssue::where('rule_code', static::class)
             ->whereNull('resolved_at')
             ->whereNotIn('validatable_id', collect($failingValidatables)->pluck('validatable_id'))
             ->update(['resolved_at' => now()]);
+    }
 
+    protected function createExecutionRecord($startedAt, int $testedCount, array $failingValidatables): ValidationExecution
+    {
         $execution = new ValidationExecution();
         $execution->rule_code = static::class;
         $execution->execution_started_at = $startedAt;
