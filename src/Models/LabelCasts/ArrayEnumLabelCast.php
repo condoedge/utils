e64050c99df056fb1390229c571d10b2743c44b3
Ezequiel <tavelliezequiel@gmail.com>
2025-09-04 17:50:35 -0300

    fix: minor fix array enum cast

--- src/Models/LabelCasts/ArrayEnumLabelCast.php
+++ src/Models/LabelCasts/ArrayEnumLabelCast.php
@@ -7,7 +7,7 @@ class ArrayEnumLabelCast extends AbstractLabelCast
     public function convert($value, $column)
     {
         if (!$value) return null;
-        $value = json_decode($value, true);
+        $value = is_array($value) ? $value : json_decode($value, true);
         
         return collect($value)->map(function ($item) use ($column) {
             return (new EnumLabelCast($this->model, $this->options))->convert($item, $column);
