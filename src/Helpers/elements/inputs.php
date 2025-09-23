9a4a6bb8682a6d172b9a532ef86e8b2c8f09ac99
Ezequiel <tavelliezequiel@gmail.com>
2025-09-23 15:51:42 -0300

    fix: GUI Issue #829  sisc

--- src/Helpers/elements/inputs.php
+++ src/Helpers/elements/inputs.php
@@ -9,18 +9,50 @@ Select::macro('overModal', function ($id = null) {
 	return $this->id($id ?? $this->label)
 		->class('select-over-modal')
 		->onFocus(fn($e) => $e->run('() => {
-		let input =  $("#' . $id . '").closest(".vlTaggableInput");
-		let inputWidth = input.width();
-		let inputOffset = input.offset();
-		let inputHeight = input.height();
-		const selectContainer = $("#' . $id . '").closest(".select-over-modal");
+			let input =  $("#' . $id . '").closest(".vlTaggableInput");
+			let inputWidth = input.width();
+			let inputOffset = input.offset();
+			let inputHeight = input.height();
+			const selectContainer = $("#' . $id . '").closest(".select-over-modal");
 
-		let style = selectContainer.attr("style") || "";
-		style += "--select-translate:" + (inputOffset.top + inputHeight) + "px !important;";
-		style += "--select-width:" + inputWidth + "px !important;";
+			let style = selectContainer.attr("style") || "";
+			style += "--select-translate:" + (inputOffset.top + inputHeight) + "px !important;";
+			style += "--select-width:" + inputWidth + "px !important;";
 
-		selectContainer.attr("style", style);
-	}'));
+			selectContainer.attr("style", style);
+		}'));
+});
+
+Dropdown::macro('dropdownOverModal', function ($id = null) {
+	$id = $id ?? (class_basename($this) . \Str::random(5) . time());
+
+	$this->elements = array_merge($this->elements ?? [], [
+		_Hidden()->onLoad(fn($e) => $e->run('() => {
+			const dropdownListenerEl = $("#' . $id . '");
+
+			function setTranslate() {
+				let dWidth = dropdownListenerEl.find(".vlDropdownMenu").width();
+				let dOffset = dropdownListenerEl.offset();
+				let dHeight = dropdownListenerEl.height();
+
+				let style = dropdownListenerEl.attr("style") || "";
+				style += "--dropdown-translate-y:" + (dOffset.top + dHeight) + "px !important;";
+
+				const rightDistance = window.innerWidth - (dOffset.left + dWidth);
+				style += "--dropdown-translate-x:" + (rightDistance) + "px !important;";
+
+				dropdownListenerEl.attr("style", style);
+			}
+
+			dropdownListenerEl.addClass("dropdown-over-modal");
+
+			dropdownListenerEl.on("mouseenter", function() {
+				setTranslate();
+			});
+	}')),
+	]);
+	
+	return $this->id($id ?? $this->label);
 });
 
 Dropdown::macro('maxHeightWithScroll', function ($height = '30rem') {
