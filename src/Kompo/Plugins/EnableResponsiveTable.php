36f3df0bb8599f05b84c7c3eb6ec53dc19718a20
Ezequiel <tavelliezequiel@gmail.com>
2025-11-14 17:33:44 -0300

    fix: Making responsive tables plugin working better

--- src/Kompo/Plugins/EnableResponsiveTable.php
+++ src/Kompo/Plugins/EnableResponsiveTable.php
@@ -7,20 +7,28 @@ use Kompo\Th;
 
 class EnableResponsiveTable extends \Condoedge\Utils\Kompo\Plugins\Base\ComponentPlugin
 {
+    protected function isDisabled()
+    {
+        return $this->getComponentProperty('isResponsive') === false
+            || !$this->componentHasMethod('headers');
+    }
+
     public function onBoot()
     {
-        if ($this->getComponentProperty('isResponsive') !== true) {
+        if ($this->isDisabled()) {
             return;
         }
 
-        $this->prependComponentProperty('class', 'responsive-table');
-    }
+        $this->prependComponentProperty('class', ' responsive-table ');
+        
+        $decoratedRows = collect(getPrivateProperty($this->getComponentProperty('query'), 'items'))->map(function ($item) {
+            return [
+                ...$item,
+                'render' => $this->decorateRow($item['render'])
+            ];
+        });
 
-    public function managableMethods()
-    {
-        return [
-            'decorateRow',
-        ];
+        setPrivateProperty($this->getComponentProperty('query'), 'items', $decoratedRows);
     }
 
     public function decorateRow($row)
@@ -29,7 +37,14 @@ class EnableResponsiveTable extends \Condoedge\Utils\Kompo\Plugins\Base\Componen
         $elements = $wrapper ? $row->elements : $row;
 
         $decoratedElements = collect($elements)->map(function ($element, $i) {
-            return $element->attr(['data-label' => $this->getHeader($i)]);
+            return _Rows(
+                _FlexBetween(
+                    !is_string($this->getHeader($i)) ? $this->getHeader($i) 
+                        : _Html($this->getHeader($i))->class('uppercase font-bold'),
+                    $element,
+                )->class('flex md:hidden'),
+                _Rows($element)->class('hidden md:block')
+            );
         });
 
         return $wrapper ? $wrapper::form($decoratedElements) : $decoratedElements;
