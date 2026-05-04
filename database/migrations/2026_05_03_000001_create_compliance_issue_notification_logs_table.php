fab535d723326c31e6a328f0048cfcd5b694031a
Ezequiel <tavelliezequiel@gmail.com>
2026-05-04 10:55:30 -0300

    Minor fix migration

--- database/migrations/2026_05_03_000001_create_compliance_issue_notification_logs_table.php
+++ database/migrations/2026_05_03_000001_create_compliance_issue_notification_logs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('compliance_issue_notification_logs', function (Blueprint $table) {
             addMetaData($table);
 
-            $table->foreignId('compliance_issue_id')->constrained('compliance_issues', 'compliance_issue_logs_compliance_issue_id_foreign')->cascadeOnDelete();
+            $table->foreignId('compliance_issue_id')->constrained('compliance_issues', indexName: 'compliance_issue_logs_compliance_issue_id_foreign')->cascadeOnDelete();
             $table->nullableMorphs('notifiable');
             $table->string('channel')->nullable();
             $table->string('recipient_label')->nullable();
