4fc1487e8f83cd9a4c8f542bcc0ac7b9b09f723a
Ezequiel <tavelliezequiel@gmail.com>
2026-05-20 16:52:08 -0300

    New warning banner

--- src/Helpers/elements/alerts.php
+++ src/Helpers/elements/alerts.php
@@ -9,3 +9,13 @@ function _CleanErrorField()
 {
     return _ErrorField()->noInputWrapper()->name('error_field', false)->class('!text-base !p-0 !m-0 !border-0 [&>.vlFieldErrors]:relative [&>.vlFieldErrors]:-top-3 [&>.vlFieldErrors]:!p-0 [&>.vlFieldErrors]:!text-base');
 }
+
+function _WarningBanner($title, $subtitle = null) {
+    return _Flex(
+        _Sax('danger', 28)->class('!text-warningdark'),
+        _Rows(
+            _Html($title)->class('!text-warningdark font-medium text-lg')->class($subtitle ? 'mb-1' : ''),
+            _Html($subtitle)->class('!text-warningdark'),
+        ),
+    )->class('flex gap-6 bg-warning/10 border-warning border p-4 rounded-lg !pr-2');
+}
